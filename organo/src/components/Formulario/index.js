bdbfc76d7714496c853e8fe8766552e489396d67
Thiago Cordeiro Dantas <thiaguinhocordeirodanttas@gmail.com>
2023-04-16 23:07:57 -0300

    fist commit

---
+++ organo/src/components/Formulario/index.js
@@ -0,0 +1,63 @@
+import CampoTexto from '../campoTexto'
+import './Formulario.css'
+import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
+import { useState } from 'react'
+
+
+const Formulario = (props) => {
+
+
+
+    const [nome, setNome] = useState('')
+    const [cargo,setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time , setTime] = useState('')
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time        
+       })
+      
+    }
+
+    return (
+        <section className="formulario">
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do colaborador.</h2>
+                <CampoTexto 
+                obrigatorio={true} 
+                label="Nome" 
+                placeholder="Digite o seu nome"
+                valor={nome}
+                aoAlterado={valor => setNome(valor)} />
+
+                <CampoTexto 
+                obrigatorio={true}  
+                label="Cargo" 
+                placeholder="Digite seu cargo" 
+                valor={cargo}
+                aoAlterado={valor => setCargo(valor)}/>
+
+                <CampoTexto     
+                label="Imagem" 
+                placeholder="Informe o endereço da imagem" 
+                valor={imagem}
+                aoAlterado={valor => setImagem(valor)}/>
+
+                <ListaSuspensa obrigatorio={true}
+                  label="Time" 
+                  itens={props.times}
+                  valor={time}
+                  aoAlterado={valor => setTime(valor)}/> 
+                <Botao texto="Criar card"/>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario 
---
+++ organo/src/components/Time/index.js
@@ -0,0 +1,14 @@
+import Colaborador from '../Colaborador'
+import './Time.css'
+
+const Time = (props) => {
+    return (
+
+        <section className='time' style={{backgroundColor: props.corSecundaria}}>
+            <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
+           <Colaborador/>
+        </section>
+    )
+}
+
+export default Time
